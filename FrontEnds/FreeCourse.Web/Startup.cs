7b67b4e3f29035acf3cc46a97bec8ab20420cc56
BurakAyan97 <burakayan97@outlook.com>
2023-06-30 23:12:30 +0300

    updated

--- FrontEnds/FreeCourse.Web/Startup.cs
+++ FrontEnds/FreeCourse.Web/Startup.cs
@@ -19,7 +19,7 @@ namespace FreeCourse.Web
         {
             Configuration = configuration;
         }
-
+        //sad mas
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
